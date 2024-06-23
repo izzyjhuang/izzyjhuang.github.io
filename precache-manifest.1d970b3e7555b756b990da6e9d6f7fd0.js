754b9ecf75a1e1f301554d9433cb9261aedae745
IzzyHuang <92131151+IzzyHuang@users.noreply.github.com>
2024-06-23 01:59:12 -0500

    Deploy

---
+++ precache-manifest.1d970b3e7555b756b990da6e9d6f7fd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94836a3bbdc86db67f403cf43095ff98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ac369ee8b9808b81ad6",
+    "url": "/static/css/main.b55ef773.chunk.css"
+  },
+  {
+    "revision": "21b82e6f65f9119b19b8",
+    "url": "/static/js/2.c15e477e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.c15e477e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6fd3f787d498c71af9",
+    "url": "/static/js/3.e53c04f3.chunk.js"
+  },
+  {
+    "revision": "1ac369ee8b9808b81ad6",
+    "url": "/static/js/main.f63d59b2.chunk.js"
+  },
+  {
+    "revision": "fc631da64bafe65604fb",
+    "url": "/static/js/runtime-main.219d43cc.js"
+  }
+]);
