bf8a53d3e8c37ec07c5f6f1e19806f7aea76d6b7
IzzyHuang <92131151+IzzyHuang@users.noreply.github.com>
2024-06-23 01:51:57 -0500

    test

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <section id="about" className="about">
       <h2>About Us</h2>
-      <p>Lavinia Lee Music Studio has been nurturing talent for over a decade. Our instructors are highly qualified and passionate about teaching music.</p>
+      <p>Lavinia Lee's Music Studio has been nurturing talent for over a decade. Our instructors are highly qualified and passionate about teaching music.</p>
     </section>
   );
 };
