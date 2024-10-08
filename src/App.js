7d8b7379b5ff1cc0088118de8d132e4f2063a3ba
izzyjhuang <izzyjhuang@gmail.com>
2024-10-07 20:57:07 -0500

    Revert "-"
    This reverts commit 80f9e15b07231da41e9fa5d3daa41acc3063631b.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
-// App.js
-import React from 'react';
+import React, { useState } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'; // Changed to HashRouter
 import Banner from './components/Banner';
 import Header from './components/Header';
 import Projects from './components/Projects';
 import Adventures from './components/Adventures';
 import AdventurePost from './components/AdventurePost';
+import Notes from './components/Notes';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
+  // State for managing the selected language
+  const [selectedLanguage, setSelectedLanguage] = useState('English');
+
   return (
     <Router>
       <div className="App">
-        <Header />
+        {/* Pass selectedLanguage and setSelectedLanguage to the Header component */}
+        <Header selectedLanguage={selectedLanguage} setSelectedLanguage={setSelectedLanguage} />
+        
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home selectedLanguage={selectedLanguage} />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/adventures" element={<Adventures />} />
+          <Route path="/adventures" element={<Adventures selectedLanguage={selectedLanguage} />} />
           <Route path="/adventures/:slug" element={<AdventurePost />} />
+          <Route path="/notes" element={<Notes selectedLanguage={selectedLanguage} />} />
           {/* Add other routes as needed */}
         </Routes>
+
         <Footer />
       </div>
     </Router>
   );
 }
 
-const Home = () => (
+// Pass selectedLanguage to the Banner component in the Home route
+const Home = ({ selectedLanguage }) => (
   <div>
-    <Banner />
+    <Banner selectedLanguage={selectedLanguage} />
     <div></div>
   </div>
 );
