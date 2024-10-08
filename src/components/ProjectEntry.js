65598b1ab1cca1e4081a3e558caec559503ca636
izzyjhuang <izzyjhuang@gmail.com>
2024-10-07 19:46:52 -0500

    -

--- src/components/ProjectEntry.js
+++ src/components/ProjectEntry.js
@@ -23,13 +23,13 @@ class ProjectEntry extends React.Component {
       ? this.props.slug // Use the slug directly for external links
       : NeedworkSlugs.includes(this.props.slug)
         ? "https://izzyjhuang.com/#/notes" // Redirect to the "Still Working" page
-        : `/adventures/${this.props.slug}`; // Redirect to the internal adventure post
+        : `${this.props.slug}`; // Redirect to the internal adventure post
 
     return (
       <div className="project-container">
         <div className="project-entry-block">
           {/* Use <a> tag for both external and internal links */}
-          <a href={linkPath} target={isExternal ? "_blank" : "_self"} rel="noopener noreferrer">
+          <a href={linkPath} target={isExternal ? "_self" : "_self"} rel="noopener noreferrer">
             <img className="project-img" src={image_path} alt={image_path} loading="lazy" />
             <div className="project-caption">{this.props.caption}</div>
             <div className="project-genre">{this.props.genre}</div>
